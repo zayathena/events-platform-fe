a13c355bbea09d6a416814ed585d73bde40779c2
Clare Regan <claremaryregan@outlook.com>
2025-05-24 12:22:22 +0200

    started building the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <p>
+          <div className="bg-red-500 text-white p-4 rounded">
+  If this is red, Tailwind is working!
+</div>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
